6402a7e35996a5d7418556dbb4be330461e80e4e
alexismaskio <alexismaskio@gmail.com>
2023-09-13 14:42:03 +0200

    Ajout de matérial design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialDesignModule} from "./material-design.module";
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import {AppRoutingModule} from "./app-routing.module";
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialDesignModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material-design.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatSlideToggleModule
+  ]
+})
+export class MaterialDesignModule { }
