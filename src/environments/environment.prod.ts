5bdaad9197b8c54f35790a8a5cc51b759a4ebe2a
alexismaskio <alexismaskio@gmail.com>
2024-02-19 10:49:00 +0100

    Save

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true,
+  production: false,
+  urlAuthApi: 'https://apidotnet.azurewebsites.net/api',
+  urlSpotifyApi: 'https://api.spotify.com',
+  urlTokenSpotifyApi: 'https://accounts.spotify.com/api/token',
+  mistralApi: "http://localhost:11434/api/generate",
 };
